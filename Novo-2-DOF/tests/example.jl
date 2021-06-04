34a9f054a6afa529331d910f2513b835052c7d2a
Pedro Puglia <pepuglia@gmail.com>
2021-06-04 09:42:18 -0300

    Testes de algumas structs

--- Novo-2-DOF/tests/example.jl
+++ Novo-2-DOF/tests/example.jl
@@ -1,1 +1,17 @@
 #tudo que precisar ser testado, fazer nessa pasta
+
+#exemplo: teste de criação das structs-base
+include("../base_def.jl")
+
+using .BaseDefinitions
+
+#overload e criação do vetor de estados
+x = StateVector(1,2,3,4,5)
+y = 2*x
+y+x
+
+#paraquedas com função de abertura
+aed = Aed(1.5, 4.5)
+drogue = Parachute(aed, (x::StateVector) -> (x.vy < 0))
+drogue.activate_condition(x)
+
