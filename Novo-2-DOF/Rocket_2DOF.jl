fa1bfb8b1ace4c19c46eb36869adc10f01829c6e
jpmoliveira <jmarchi.oliveira@gmail.com>
2021-06-09 07:15:36 -0300

    [Redacted]
    [Redacted]

--- Novo-2-DOF/Rocket_2DOF.jl
+++ Novo-2-DOF/Rocket_2DOF.jl
@@ -4,15 +4,11 @@ module Rocket_2DOF
 #exportar menos coisas? (só StateVector, Rkt e Amb)
 export StateVector, Aed, Parachute, Propulsion, Rocket, Rail, Environment
 
-# Rail Dynamics
-export forces_rail
-
 # Runge-Kutta
 # Se pa n precisa exportar o rk4solver
 export rk4solver, rk4solution, rk4flight
 
 include("base_def.jl")
-include("rail_dynamic.jl")
 include("rk4solve.jl")
 
 end # module
