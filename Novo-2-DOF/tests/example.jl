813de4acdf57b46c8402d2c96e957e8ab994b19b
Walter <walterarbiol@gmail.com>
2021-06-11 17:39:39 -0300

    teste

--- Novo-2-DOF/tests/example.jl
+++ Novo-2-DOF/tests/example.jl
@@ -1,17 +1,25 @@
 #tudo que precisar ser testado, fazer nessa pasta
 
 #exemplo: teste de criação das structs-base
-include("../base_def.jl")
+include("../rail_dynamic.jl")
 
 using .BaseDefinitions
 
-#overload e criação do vetor de estados
-x = StateVector(1,2,3,4,5)
-y = 2*x
-y+x
+X₀ = StateVector(0, 1294, 13, 34.3, 4.5)
 
-#paraquedas com função de abertura
-aed = Aed(1.5, 4.5)
-drogue = Parachute(aed, (x::StateVector) -> (x.vy < 0))
-drogue.activate_condition(x)
+aed_drogue = Aed(1.5, 0.7)
+drogue = Parachute(aed_drogue, (x::StateVector) -> x.vy < 0)
 
+aed_main = Aed(1.5, 4)
+main = Parachute(aed_drogue, (x::StateVector) -> x.y < 1294+300)
+
+motor = Propulsion(400, 4.5, 6.5)
+
+#mudar a forma de armazenamento da condição de voo
+rocket = Rocket(23.5, motor, Aed(0.4, 0.08), drogue, main, "Rail")
+
+rail = Rail(5, 85, 0.03)
+
+env = Environment(9.81, 1.225, rail, 1294)
+
+forces_rail(X₀, 0.0, env, rocket)
