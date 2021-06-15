2b46f3f667480bd2799a8f022f9af6d6043abf53
Walter <walterarbiol@gmail.com>
2021-06-15 17:22:05 -0300

    troca de velocidade para tempo

--- Novo-2-DOF/Thrust.jl
+++ Novo-2-DOF/Thrust.jl
@@ -7,12 +7,10 @@ function currentThrust(X::StateVector, thrust::Float64)
 end
 
 function currentThrust(X::StateVector, thrust::Array{Float64})
-    v=sqrt(X.vx^2+X.vy^2)                 #velocidade do foguete
-     
     for i in 1:(size(thrust)[1])            
-        if v==thrust[i, 2] return thrust[i, 1] end   #thrust[i, 1]=thrust ||||thrust[i, 2]=velocidade 
-        if (v > thrust[i, 2]) && (v < thrust[i+1, 2])
-            return thrust[i, 1]+ (v-thrust[i, 2])*(thrust[i+1,1]-thrust[i,1])/(thrust[i+1,2]-thrust[i,2]) #formula descoberta pela equação da reta
+        if t==thrust[i, 2] return thrust[i, 1] end   #thrust[i, 1]=thrust ||||thrust[i, 2]=tempo 
+        if (t > thrust[i, 2]) && (t < thrust[i+1, 2])
+            return thrust[i, 1]+ (t-thrust[i, 2])*(thrust[i+1,1]-thrust[i,1])/(thrust[i+1,2]-thrust[i,2]) #formula descoberta pela equação da reta
         end
     end
 end
