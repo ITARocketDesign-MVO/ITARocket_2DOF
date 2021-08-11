b69d25fab3eafa689199461ecbf942fbca4c32f0
Francisco Eduardo Fontenele <83652904+bigchico25@users.noreply.github.com>
2021-08-11 14:03:28 -0300

    Add files via upload
    Corrigido

---
+++ Novo-2-DOF/dyna_main.jl
@@ -0,0 +1,18 @@
+include("base_def.jl")
+
+using .BaseDefinitions
+
+function forces_main(X::StateVector, t::Float64, env::Enviroment, rocket::Rocket)
+    
+    M = X.m_comb + rocket.empty_mass
+    W = M * env.g
+    cosθ = X.vx/sqrt(X.vx^2 + X.vy^2)
+    sinθ = X.vy/sqrt(X.vx^2 + X.vy^2)
+
+    Drag = 0.5 * env.ρ * rocket.main.aed.area * M * (X.vx^2 + X.vy^2)
+
+    Fx = -cosθ * Drag
+    Fy = -sinθ * Drag - W
+
+    return Fx, Fy 
+end
