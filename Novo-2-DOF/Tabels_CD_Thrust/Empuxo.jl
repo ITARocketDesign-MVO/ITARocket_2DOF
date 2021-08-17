be74e28ec85007d4a8bb1f74eeeefd8acfabaec9
WArbiol <walterarbiol@gmail.com>
2021-08-17 12:28:17 -0300

    Fazendo as funções para leitura

--- Novo-2-DOF/Tabels_CD_Thrust/Empuxo.jl
+++ Novo-2-DOF/Tabels_CD_Thrust/Empuxo.jl
@@ -1,13 +1,14 @@
-tempo = []    #vetor nulo
-Empuxo = []    #vetor nulo
+function Leithrust(arquivo::String)
+    tempo = []    #vetor nulo
+    Empuxo = []    #vetor nulo
 
-x = open("Empuxo_completo.dat","r") do x
-    for i in eachline(x) 
-        numeros = rsplit(i, "\t")                   #Separa a string em um vetor de string (obs.: separa a string de acordo com "\t")
-        push!(tempo, parse(Float64, numeros[1]));   #Push insere um elemento na próxima linha de uma matriz com uma coluna, push!(matriz, elemento)
-        push!(Empuxo, parse(Float64, numeros[2]));  #o elemento é o parse(...), parse serve pra transformar: parse(o formato que tu quer, o que você quer transformar)
-    end                                             #no caso transforma string para float
+    x = open(arquivo,"r") do x
+        for i in eachline(x) 
+            numeros = rsplit(i, "\t")                   #Separa a string em um vetor de string (obs.: separa a string de acordo com "\t")
+            push!(tempo, parse(Float64, numeros[1]));   #Push insere um elemento na próxima linha de uma matriz com uma coluna, push!(matriz, elemento)
+            push!(Empuxo, parse(Float64, numeros[2]));  #o elemento é o parse(...), parse serve pra transformar: parse(o formato que tu quer, o que você quer transformar)
+        end                                             #no caso transforma string para float
+    end
+    thrust = hcat(tempo, Empuxo)  #concatena duas matrizes no  sentido das colunas 
+    return thrust;
 end
-
-
-thrust = hcat(tempo, Empuxo)    #concatena duas matrizes no  sentido das colunas
